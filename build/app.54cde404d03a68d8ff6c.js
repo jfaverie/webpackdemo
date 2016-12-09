94acbcd837254515467a113c763b58668c5d4b8b
malbert@excilys <malbert.excilys@desktop-0130.ebi.excilys.com>
2016-12-09 17:03:49 +0100

    First commit

---
+++ build/app.54cde404d03a68d8ff6c.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],[function(n,e,o){var t=o(1);o(2),document.body.appendChild(t())},function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world",n}}]);
+//# sourceMappingURL=app.54cde404d03a68d8ff6c.js.map
